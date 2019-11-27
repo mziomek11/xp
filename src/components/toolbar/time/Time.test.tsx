1deae63adba5671301882032c3a6c76626c7cc05
hommat <zmateusz1111@gmail.com>
2019-11-27 09:28:27 +0100

    Add toolbar time component timer functionality

--- src/components/toolbar/time/Time.test.tsx
+++ src/components/toolbar/time/Time.test.tsx
@@ -4,12 +4,22 @@ import { shallow } from "enzyme";
 import Time from "./Time";
 import { findByTestAtrr } from "../../../../testingUtils";
 
-const wrapper = shallow(<Time />);
+const wrapper = shallow<Time>(<Time />);
+const instance = wrapper.instance();
 
 describe("Time Component", () => {
   describe("render", () => {
     it("should render without throwing an error", () => {
       expect(findByTestAtrr(wrapper, "time").length).toBe(1);
+    });
+  });
+
+  describe("updateTime", () => {
+    it("should update state", () => {
+      instance.setState({ time: "time" });
+      instance.updateTime();
+
+      expect(instance.state.time).not.toBe("time");
     });
   });
 });
--- src/components/toolbar/time/Time.tsx
+++ src/components/toolbar/time/Time.tsx
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+
 import { toolbarConfig } from "../../../config";
 
-const Time = () => {
-  return (
-    <div
-      className="time"
-      data-test="time"
-      style={{ width: toolbarConfig.TIME_WIDTH }}
-    >
-      13:37
-    </div>
-  );
+type State = {
+  time: string;
 };
+
+//source https://stackoverflow.com/questions/19407305/how-to-show-only-hours-and-minutes-from-javascript-date-tolocaletimestring
+function getCurrentDate() {
+  const date = new Date();
+  const localeSpecificTime = date.toLocaleTimeString();
+  return localeSpecificTime.replace(/:\d+ /, " ");
+}
+
+class Time extends Component<{}, State> {
+  private interval: NodeJS.Timeout | null = null;
+  readonly state: State = {
+    time: getCurrentDate()
+  };
+
+  componentDidMount() {
+    const interval = setInterval(this.updateTime, 3000);
+    this.interval = interval;
+  }
+
+  componentWillUnmount() {
+    if (this.interval) clearInterval(this.interval);
+  }
+
+  updateTime = () => {
+    const newTime = getCurrentDate();
+    if (newTime !== this.state.time) this.setState({ time: newTime });
+  };
+
+  render() {
+    return (
+      <div
+        className="time"
+        data-test="time"
+        style={{ width: toolbarConfig.TIME_WIDTH }}
+      >
+        {this.state.time}
+      </div>
+    );
+  }
+}
 
 export default Time;
